c6a13cfe22dc254d2a2cb3ad20ff34b545e67720
Joshua Bagley <JoshBagley@gmail.com>
2017-12-13 19:10:40 -0500

    Unity Update
    Unity Update

--- Assets/Scripts/view/HudView.cs
+++ Assets/Scripts/view/HudView.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
+
 
 class HudView : MonoBehaviour
 {
@@ -10,6 +12,25 @@ class HudView : MonoBehaviour
 
     public void progressTurn()
     {
+        Text objPlayerTitle = GameObject.Find("txtPlayerName").GetComponent<Text>();
+        Button btnOption1 = GameObject.Find("btnOption1").GetComponent<Button>();
+
+        btnOption1.onClick.RemoveAllListeners();
+
+        if (objPlayerTitle.text.Equals("Player 1",StringComparison.CurrentCultureIgnoreCase)) {
+            objPlayerTitle.text = "Player 2";
+            btnOption1.onClick.AddListener(() => { HideButton(btnOption1); });
+            btnOption1.gameObject.SetActive(true);
+        } else
+        {
+            objPlayerTitle.text = "Player 1";
+            btnOption1.gameObject.SetActive(false);
+        }
         //next turn logic here...
+    }
+
+    public void HideButton(Button btnButton)
+    {
+        btnButton.enabled = false;
     }
 }
